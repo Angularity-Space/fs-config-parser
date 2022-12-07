67c5999ded19a869e90910dfc169b2181009ff1a
aag_8a <72987745+aag8a@users.noreply.github.com>
2022-12-07 14:21:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,80 @@
-import qdarkstyle, os, shutil, json
+import os, shutil, json, qdarkstyle, sys, webbrowser
 from PyQt6 import QtWidgets, uic
-from PyQt6.QtWidgets import QListWidgetItem
+from PyQt6.QtWidgets import QListWidgetItem, QFileDialog, QMessageBox
+from PyQt6.QtGui import QFont, QFontDatabase
+
 app = QtWidgets.QApplication([])
-configparse = uic.loadUi("configparser.ui")
-values = uic.loadUi('value.ui')
+ui = uic.loadUi(r"E:\Codes\RC\FSCP\main.ui")
+edit = uic.loadUi(r"E:\Codes\RC\FSCP\value.ui")
 app.setStyleSheet(qdarkstyle.load_stylesheet())
-def openButtonCFG():
-    os.system("explorer " + savefolder)
-configparse.show()
+families = QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont('segoe_ui_var.ttf'))
 if os.name == "posix":
     if os.path.exists(f"/home/{os.getlogin()}/.config/unity3d/Haze Games/Fractal Space/FractalSave.dat"):
-        savefolder = f"/home/{os.getlogin()}/.config/unity3d/Haze Games/Fractal Space/"
-        savefolder = os.path.normpath(savefolder)
+        defaultFolder = f"/home/{os.getlogin()}/.config/unity3d/Haze Games/Fractal Space/"
+        defaultFolder = os.path.normpath(defaultFolder)
+        ui.folderStatus.setText("Found")
     else:
-        configparse.configT.setText("Missing")
+        ui.folderStatus.setText("Not found")
 else:
     if os.path.exists(os.getenv("UserProfile") + "\AppData\LocalLow\Haze Games\Fractal Space"):
-        savefolder = os.getenv("UserProfile") + "\AppData\LocalLow\Haze Games\Fractal Space"
-        savefolder = os.path.normpath(savefolder)
+        defaultFolder = os.getenv("UserProfile") + "\AppData\LocalLow\Haze Games\Fractal Space"
+        defaultFolder = os.path.normpath(defaultFolder)
+        ui.folderStatus.setText("Found")
     else:
-        configparse.configT.setText("Missing")
-configparse.configT.setText("Found")
-configparse.openB.setDisabled(False)
-configparse.groupBox_2.setDisabled(False)
-configparse.openB.clicked.connect(openButtonCFG)
-if not os.path.exists(savefolder + "\FractalSave.bk"):
-    shutil.copyfile(savefolder + "/FractalSave.dat", savefolder + "/FractalSave.bk")
-with open(savefolder + "/FractalSave.dat", "r") as cfg:
-    data = json.load(cfg)
-    for i in range(len(data['keys'])):
-        configparse.keyE.addItem(QListWidgetItem(data['keys'][i]))
-        cfg.close()
+        ui.folderStatus.setText("Not found")
 
+def selfile():
+    global save, data
+    save = QFileDialog.getOpenFileName(None, "Fractal Save", defaultFolder, "Data file (*.dat)")
+    if save == '':
+        pass
+    else:
+        try:
+            with open(save[0], "r") as cfg:
+                data = json.load(cfg)
+                for i in range(len(data['keys'])):
+                    ui.content.addItem(QListWidgetItem(data['keys'][i]))
+                    cfg.close()
+        except json.decoder.JSONDecodeError:
+            dialog = QMessageBox()
+            dialog.setWindowTitle("Error")
+            dialog.setIcon(QMessageBox.Icon.Critical)
+            dialog.setText("JSON Decoder Error! Error code: 15")
+            dialog.exec()
+        except KeyError:
+            dialog = QMessageBox()
+            dialog.setWindowTitle("Error")
+            dialog.setIcon(QMessageBox.Icon.Critical)
+            dialog.setText("Reading failed! Error code: 16")
+            dialog.exec()           
+        except FileNotFoundError:
+            pass                 
+if not os.path.exists(defaultFolder + "\FractalSave.bk"):
+    shutil.copyfile(defaultFolder + "/FractalSave.dat", defaultFolder + "/FractalSave.bk")
 def editButton():
-    values.show()
-    values.keyL.setText(data['keys'][configparse.keyE.currentRow()])
-    values.valueL.setText(data['values'][configparse.keyE.currentRow()])
+    edit.show()
+    edit.keyL.setText(data['keys'][ui.content.currentRow()])
+    edit.valueL.setText(data['values'][ui.content.currentRow()])
 def okayButton():
-    data['values'][configparse.keyE.currentRow()] = values.valueL.text()
-    with open(savefolder + "\FractalSave.dat", "w") as changedCFG: 
+    data['values'][ui.content.currentRow()] = edit.valueL.text()
+    with open(defaultFolder + "\FractalSave.dat", "w") as changedCFG: 
         json.dump(data, changedCFG, ensure_ascii=False)
         changedCFG.close()
-    values.close()
-configparse.changeB.clicked.connect(editButton)
-configparse.keyE.itemClicked.connect(lambda: configparse.changeB.setDisabled(False))
-values.cancel.clicked.connect(lambda: values.close())
-values.OK.clicked.connect(okayButton)
+    edit.close()
 
+def requestSave():
+    shutil.copyfile(defaultFolder + "/FractalSave.dat", defaultFolder + "/FractalSave.bk")
+    dialog = QMessageBox()
+    dialog.setWindowTitle("Backup")
+    dialog.setIcon(QMessageBox.Icon.Information)
+    dialog.setText("New backup saved!")
+    dialog.exec()
+
+ui.opensave.clicked.connect(selfile)
+ui.content.itemDoubleClicked.connect(editButton)
+ui.request_backup.triggered.connect(requestSave)
+ui.actionError_codes.triggered.connect(lambda: webbrowser.open("https://github.com/Angularity-Space/fs-config-parser/tree/2.0-rewroten"))
+edit.cancel.clicked.connect(lambda: edit.close())
+edit.OK.clicked.connect(okayButton)
+ui.show()
 app.exec()
